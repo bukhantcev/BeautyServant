b08b80931a0bcd55ae59adfb4b4ed9e2fad77214
bukhantcev <bukhantcev@mail.ru>
2023-11-23 13:56:55 +0300

    22.11.23

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import logging
-
-import asyncio
 import os
 
 from aiogram import Bot, Dispatcher, executor, types
 
 API_TOKEN = os.getenv('TOKEN')
-chat_id_admin = -1001911875283
+#chat_id_admin = -1001911875283
 
 
 logging.basicConfig(level=logging.INFO)
@@ -14,45 +12,37 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN, parse_mode='html')
 dp = Dispatcher(bot)
 
-'''@dp.message_handler()
-async def allll(message: types.Message):
-    print(message.from_user.id)'''
 
 
 @dp.message_handler(content_types="new_chat_members")
 async def on_user_join(message: types.Message):
-    if message.chat.id == chat_id_admin:
-        print(message.chat.id)
-        await message.delete()
-        await bot.send_message(text='Bot rabotaet!))', chat_id= 404354012)
+    print(message.chat.id)
+    await message.delete()
+    await bot.send_message(text='Bot rabotaet!))', chat_id= 404354012)
 
 @dp.message_handler(content_types="left_chat_member")
 async def on_user_join(message: types.Message):
-    if message.chat.id == chat_id_admin:
-        print(message.chat.id)
-        await message.delete()
-        await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
+    print(message.chat.id)
+    await message.delete()
+    await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
 
 @dp.message_handler(content_types="new_chat_title")
 async def on_user_join(message: types.Message):
-    if message.chat.id == chat_id_admin:
-        print(message.chat.id)
-        await message.delete()
-        await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
+    print(message.chat.id)
+    await message.delete()
+    await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
 
 @dp.message_handler(content_types="new_chat_photo")
 async def on_user_join(message: types.Message):
-    if message.chat.id == chat_id_admin:
-        print(message.chat.id)
-        await message.delete()
-        await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
+    print(message.chat.id)
+    await message.delete()
+    await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
 
 @dp.message_handler(content_types="delete_chat_photo")
 async def on_user_join(message: types.Message):
-    if message.chat.id == chat_id_admin:
-        print(message.chat.id)
-        await message.delete()
-        await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
+    print(message.chat.id)
+    await message.delete()
+    await bot.send_message(chat_id=404354012, text='Bot rabotaet!))')
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
